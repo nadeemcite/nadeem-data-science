12a8f21c33022c45bdfc1cd1ade10a959cb36ae9
nadeemcite <nady5730642@gmail.com>
2018-02-09 23:34:07 +0530

    index

--- dataapp/settings.py
+++ dataapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')cle5_i5fzqtn4se%sd&6s&+cns5e)l15@)kltl=vv+3+81@+7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
